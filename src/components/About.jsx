b70ccfaf0c9b785356c1f74356684a0c89a3846e
Caleb <cco139@uclive.ac.nz>
2026-02-26 16:06:20 +1300

    Small change updating about section to final year student

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <section id="about" className="about">
       <h2 style={{ letterSpacing: 3 }}>About Me</h2>
-      <p>Hi! I am currently a third year Software Engineering student at the University of Canterbury. This website shows off a collection 
+      <p>Hi! I am currently a final year Software Engineering student at the University of Canterbury. This website shows off a collection
         of my personal and university projects!</p>
     </section>
   );
